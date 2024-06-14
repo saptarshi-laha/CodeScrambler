5705639c22c6d4568144856c8faa9c24bbee9232
Saptarshi Laha <saptarshi.laha@gmail.com>
2024-06-15 00:37:35 +0100

    Added Positional Data to Dictionary for Junk Code and Jump Injection

--- CodeScrambler.py
+++ CodeScrambler.py
@@ -76,42 +76,77 @@ def disassemble_machine_code(code, pe_type, pe_oep, pe_ib, pe_oep_section, pe_oe
     return status_success, output, oep_index
 
 
-def add_instructions_and_add_jump_positional_indicators(disassembled_machine_code, pe_type, oep_index, complexity):
-    x64_instructions = [('90', 1), ('4889c0', 3), ('5058', 2), ('488d4000', 4)]
-    x86_instructions = [('90', 1), ('89c0', 2), ('5058', 2), ('8d4000', 3)]
+def add_instructions_and_add_jump_positional_indicators(disassembled_machine_code, pe_type):
+    x64_instructions = []
+    x86_instructions = []
+    instruction_count = 0
 
-    # The aim for the above is to be imported from a different pre-defined list.
-    # These are just examples above used for testing. This code will be added in later.
+    if pe_type == 0x10b:
+        with open('x86_instructions.txt', 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line:
+                    parts = line.replace('(', '').replace(')', '').split(',')
+                    instruction = (parts[0].strip().strip("'"), int(parts[1].strip()))
+                    x86_instructions.append(instruction)
+                    instruction_count += 1
+    elif pe_type == 0x20b:
+        with open('x64_instructions.txt', 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line:
+                    parts = line.replace('(', '').replace(')', '').split(',')
+                    instruction = (parts[0].strip().strip("'"), int(parts[1].strip()))
+                    x64_instructions.append(instruction)
+                    instruction_count += 1
 
+    for section_name, instructions in disassembled_machine_code.items():
+        total_instructions_in_section = len(instructions) * (instruction_count + 3)
+        updated_instructions = {}
+        for index, (_, _, _) in enumerate(instructions.values()):
+            for i in range(0, total_instructions_in_section):
+                if i % (instruction_count + 3) == instruction_count + 2:
+                    updated_instructions[i] = instructions[index]
+                elif i % (instruction_count + 3) == instruction_count + 1:
+                    updated_instructions[i] = (-3,-3,-3)
+                elif i % (instruction_count + 3) == instruction_count:
+                    updated_instructions[i] = (-2,-2,-2)
+                else:
+                    updated_instructions[i] = (-1, -1, -1)
+        disassembled_machine_code[section_name] = updated_instructions
+
+    print(disassembled_machine_code)
 
 def main():
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
         print(
-            "Usage: python3 CodeScrambler.py <pefile> <complexity>\nThe complexity argument is a number between 0 and "
+            "Usage: python3 CodeScrambler.py <pefile> <junk_instruction_complexity> <jump_complexity> <conditional jump on(1)/off(0)>\nThe <junk_instruction_complexity> argument is a number between 0 and "
             "N which indicates the maximum number of junk arguments inserted into machine code between consecutive "
             "instructions.\nThe selection of junk opcodes is random and so is the number of junk arguments, "
             "but this number will never exceed complexity.\nIt is also important to remember that the complexity "
             "cannot exceed the number of junk instructions provided in the list.")
+        exit(1)
     else:
         filepath = sys.argv[1]
-        complexity_percent = int(sys.argv[2])
+        complexity_junk_instr = int(sys.argv[2])
+        complexity_jmp = int(sys.argv[3])
+        conditional_jmp = int(sys.argv[4])
         success, code, pe_type, pe_oep, pe_ib, pe_oep_section, pe_oep_section_va = read_pe_file(filepath)
 
         if success:
             print("Successfully loaded and read PE file")
             success, output, oep_index = disassemble_machine_code(code, pe_type, pe_oep, pe_ib, pe_oep_section,
                                                                   pe_oep_section_va)
-            print(output)
             if success:
                 print("Successfully disassembled instructions")
-                add_instructions_and_add_jump_positional_indicators(output, pe_type, oep_index, complexity_percent)
+                add_instructions_and_add_jump_positional_indicators(output, pe_type)
             elif not success:
                 print("Failed to disassemble instructions")
-                return 1
+                exit(1)
 
         elif not success:
             print("Failed to load and read PE file")
-            return 1
+            exit(1)
 
 
 if __name__ == "__main__":
